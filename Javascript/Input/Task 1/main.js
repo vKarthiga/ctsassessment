8fa08bd616273149ad62d0dfa6b2ce6c9abf7fb4
vKarthiga <karthigavenkat971@gmail.com>
2025-05-27 15:52:14 +0530

    Create main.js

---
+++ Javascript/Input/Task 1/main.js
@@ -0,0 +1,7 @@
+// Log a welcome message to the console
+console.log("Welcome to the Community Portal");
+
+// Alert when the page is fully loaded
+window.addEventListener('load', () => {
+  alert("Page fully loaded. Welcome!");
+});
